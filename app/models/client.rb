6303efb7a90bf1c1065e382e64f7166741e595e4
Jamshaid Rehman <jamshaidr0@gmail.com>
2021-07-29 12:20:28 +0500

    Add client crud

---
+++ app/models/client.rb
@@ -0,0 +1,6 @@
+class Client < ApplicationRecord
+
+  validates :name, :email, :contact, :address, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :contact, length: {maximum: 11}
+end
